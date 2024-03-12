0850728f7e96d431e7f4a60130a0ab7ce6fec2d1
abrar2139 <mabrarqadri3@mail.com>
2024-03-12 16:20:54 +0500

    https changes

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("chattings-backend.vercel.app", {
+			const socket = io("https://chattings-backend.vercel.app", {
 				query: {
 					userId: authUser._id,
 				},
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "chattings-backend.vercel.app",
+				target: "https://chattings-backend.vercel.app",
 			},
 		},
 	},
